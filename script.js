91a132ac4d5de1dec15af574b0aa6a76809faad9
tool <tokumei24yayayu@gmail.com>
2024-11-04 23:10:14 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function convertLink() {
+    const originalLink = document.getElementById('originalLink').value;
+    const linkType = document.getElementById('linkType').value;
+    const regex = /ticket=([^&]+)/;
+    const match = originalLink.match(regex);
+
+    if (match && match[1]) {
+        const ticket = match[1];
+        const convertedLink = `line://square/${linkType}?ticket=${ticket}`;
+        document.getElementById('convertedLink').textContent = convertedLink;
+    } else {
+        document.getElementById('convertedLink').textContent = "リンク形式が無効です。もう一度試してください。";
+    }
+}
